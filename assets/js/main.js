c3005e94c522bdf501f0afd12d580c7ad7f7cc82
Syarif Rahman <yourbaerip@gmail.com>
2024-12-23 04:26:58 +0800

    add service description

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,3 +175,22 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+(function() {
+            // https://dashboard.emailjs.com/admin/account
+            emailjs.init({
+              publicKey: "YOUR_PUBLIC_KEY",
+            });
+})();
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(() => {
+                        console.log('SUCCESS!');
+                    }, (error) => {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
